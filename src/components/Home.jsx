18d5b4d431e88d392d2f8aa71d872b357ba1a3c0
Sanmayu1 <nandipatis4941@my.sduhsd.net>
2026-03-16 13:28:24 -0700

    3/16 Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,15 @@ import Card from './Card.jsx'
 
 function Home() {
   return (
+    
     <div className="cards-container">
+      <h1>Welcome To The Fruit Store!! 
+        Have Fun!!
+      </h1>
       <Card name="Apple" description="A juicy red apple, perfect for snacking daily." />
       <Card name="Banana" description="A sweet yellow banana, great for quick energy." />
       <Card name="Orange" description="A fresh orange full of vitamin C goodness." />
+    
     </div>
   )
 }
